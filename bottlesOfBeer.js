d399090a3413f15585aac978c3d8c1c2e34c92a6
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-04 19:22:03 +0100

    Create bottlesOfBeer.js
    Create: started initial coding task - think improvement can be made

---
+++ bottlesOfBeer.js
@@ -0,0 +1,16 @@
+//Create - program which prints complete lyrics to 99 bottles of beer
+
+var beers = 99
+while (beers > 0) {
+  console.log(beers + " bottles of beer on the wall");
+  console.log(beers + " bottles of beer");
+  console.log(beers + " Take one down, pass it around")
+  beers = count -1;
+  if (count > 0) {
+    console.log(beers + "bottles of beer on the wall");
+  } else {
+    console.log("No more bottles of beer on the wall");
+  }
+}
+    
+    // think I can improve on this
