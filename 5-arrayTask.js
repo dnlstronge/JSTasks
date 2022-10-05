35f8e8a43f4ebeb6823c6d492bf847084f969f11
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-05 09:26:42 +0100

    Update 5-arrayTask.js
    fix: added loops
    test: functions as expected

--- 5-arrayTask.js
+++ 5-arrayTask.js
@@ -1,4 +1,15 @@
 // create a nested array which compares two sets of followers to find mutual values
 
 const bobsFollowers = ['dave', 'mick', 'steve', 'bert'];
-const tinasFollowers = 
+const tinasFollowers = ['dave', 'mick', 'joan'];
+const mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+for (let j = 0; j < tinasFollowers.length; j++) {
+    if(bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i])
+    }
+  }
+};
+
+//tested - functional
